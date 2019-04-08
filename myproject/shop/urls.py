23054ad62bb4bef279843384bffc5e6844f9ae23
developerjaydndplough <47351433+developerjaydndplough@users.noreply.github.com>
2019-04-08 16:03:59 +0530

    8/4/2019

--- myproject/shop/urls.py
+++ myproject/shop/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('about/', views.about, name='About'),
     path('contact/', views.contact, name='Contact'),
     path('tracker/', views.tracker, name='TrackingStatus'),
-    path('search/', views.search, name='Search'),
+    path('search/', views.search, name='search'),
     path('checkout/', views.checkout, name='Checkout'),
     path("handlerequest/", views.handlerequest, name="HandleRequest"),
 ]
--- myproject/shop/views.py
+++ myproject/shop/views.py
@@ -40,25 +40,34 @@ def searchMatch(query, item):
     if query in item.desc.lower() or query in item.product_name.lower() or query in item.category.lower():
         return True
     else:
-    	return False
+        return False
 
 def search(request):
-    query = request.GET.get('search')
-    allProds = []
-    catprods = Product.objects.values('category', 'id')
-    cats = {item['category'] for item in catprods}
-    for cat in cats:
-        prodtemp = Product.objects.filter(category=cat)
-        prod = [item for item in prodtemp if searchMatch(query, item)]
+	query = request.POST.get('search')
+	category = []
+	c = Product.objects.values('category')
+	for x in c:
+		if(x not in category):
+			category.append(x)
+	
+	allProds = []
+	catprods = Product.objects.values('category', 'id')
+	cats = {item['category'] for item in catprods}
+	for cat in cats:
+		prodtemp = Product.objects.filter(category=cat)
+		prod = [item for item in prodtemp if searchMatch(query, item)]
+		n = len(prod)
+		nSlides = n // 4 + ceil((n / 4) - (n // 4))
+		prod=list(prod)
+		prod.reverse()
+		if len(prod) != 0:
+			allProds.append([prod, range(1, nSlides), nSlides, n])
+	dic = {'allProds': allProds, "msg": "", 'category':category}
+	if len(allProds) == 0 or len(query)<4:
+		dic = {'msg': "Please make sure to enter relevant search query", 'category':category}
+	return render(request, 'shop/search.html', dic)
 
-        n = len(prod)
-        nSlides = n // 4 + ceil((n / 4) - (n // 4))
-        if len(prod) != 0:
-            allProds.append([prod, range(1, nSlides), nSlides])
-    params = {'allProds': allProds, "msg": ""}
-    if len(allProds) == 0 or len(query)<4:
-        params = {'msg': "Please make sure to enter relevant search query"}
-    return render(request, 'shop/search.html', params)
+
 
 def about(request):
 	return render(request, 'shop/about.html')
